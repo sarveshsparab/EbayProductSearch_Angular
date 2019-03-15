c2af57a140412a04edf1bf2f84511846fd81b698
Sarvesh Parab <sparab@usc.edu>
2019-03-15 16:33:03 -0700

    form module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { ProdFormComponent } from './prod-form/prod-form.component';
+import { CatToValPipe } from './pipes/cat-to-val.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProdFormComponent
+    ProdFormComponent,
+    CatToValPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
