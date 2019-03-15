53789c81f24f48a22bd3eac4dc9ee3b53368270e
Sarvesh Parab <sparab@usc.edu>
2019-03-14 21:16:40 -0700

    product form const

---
+++ src/app/prod-form/prod-form.ts
@@ -0,0 +1,11 @@
+export const ProductForm = {
+  keyword: '',
+  category: 'all',
+  miles: '',
+  condNew: false,
+  condUsed: false,
+  condUnspecified: false,
+  localPickup: false,
+  freeShipping: false,
+  zipCode: ''
+};
