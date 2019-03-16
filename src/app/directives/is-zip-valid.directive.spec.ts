747b30bd3a2cd7e43637490a3dcda7615e6fbddc
Sarvesh Parab <sparab@usc.edu>
2019-03-15 23:48:27 -0700

    custom zip code vaildator directive - isValidZip

---
+++ src/app/directives/is-zip-valid.directive.spec.ts
@@ -0,0 +1,8 @@
+import { IsZipValidDirective } from './is-zip-valid.directive';
+
+describe('IsZipValidDirective', () => {
+  it('should create an instance', () => {
+    const directive = new IsZipValidDirective();
+    expect(directive).toBeTruthy();
+  });
+});
