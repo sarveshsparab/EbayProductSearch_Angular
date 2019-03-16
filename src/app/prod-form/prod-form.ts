77d1ce45f9ef53beeaef710cc19a87ed50bd98b7
Sarvesh Parab <sparab@usc.edu>
2019-03-15 19:17:26 -0700

    default form values set

--- src/app/prod-form/prod-form.ts
+++ src/app/prod-form/prod-form.ts
@@ -1,13 +1,13 @@
 export const ProductForm = {
   keyword: '',
-  category: 'all',
+  category: -1,
   miles: '',
   condNew: false,
   condUsed: false,
   condUnspecified: false,
   localPickup: false,
   freeShipping: false,
-  zipCodeType: '',
+  zipCodeType: 'curr',
   currZipCode: '',
   custZipCode: ''
 };
