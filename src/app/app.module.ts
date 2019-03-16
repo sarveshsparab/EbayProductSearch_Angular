5db573f5e91db298db57afea881198ead92c979e
Sarvesh Parab <sparab@usc.edu>
2019-03-16 16:56:56 -0700

    new modules added for autocomplete... but autocompete doesnt work as of yet!!!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { FormsModule } from '@angular/forms';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+
+import { BrowserModule } from '@angular/platform-browser';
+import {MatButtonModule, MatFormFieldModule, MatInputModule, MatNativeDateModule, MatRippleModule} from '@angular/material';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 
 import { AppComponent } from './app.component';
 import { ProdFormComponent } from './prod-form/prod-form.component';
 import { CatToValPipe } from './pipes/cat-to-val.pipe';
 import { IsEmptyDirective } from './directives/is-empty.directive';
 import { IsZipValidDirective } from './directives/is-zip-valid.directive';
+import { ZipCodeAutoCompleteDirective } from './directives/zip-code-auto-complete.directive';
 
 @NgModule({
   declarations: [
@@ -17,12 +21,27 @@ import { IsZipValidDirective } from './directives/is-zip-valid.directive';
     ProdFormComponent,
     CatToValPipe,
     IsEmptyDirective,
-    IsZipValidDirective
+    IsZipValidDirective,
+    ZipCodeAutoCompleteDirective
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
