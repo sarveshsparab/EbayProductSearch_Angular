db115db3625426a64152b3155207590574847650
Sarvesh Parab <sparab@usc.edu>
2019-03-16 20:42:31 -0700

    added prod results componenet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,13 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 
 import { AppComponent } from './app.component';
-import { ProdFormComponent } from './prod-form/prod-form.component';
+import { ProdFormComponent } from './components/prod-form/prod-form.component';
 import { CatToValPipe } from './pipes/cat-to-val.pipe';
 import { IsEmptyDirective } from './directives/is-empty.directive';
 import { IsZipValidDirective } from './directives/is-zip-valid.directive';
 import { ZipCodeAutoCompleteDirective } from './directives/zip-code-auto-complete.directive';
 import { DisableZipCodeDirective } from './directives/disable-zip-code.directive';
+import { ProdResultsComponent } from './components/prod-results/prod-results.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { DisableZipCodeDirective } from './directives/disable-zip-code.directive
     IsEmptyDirective,
     IsZipValidDirective,
     ZipCodeAutoCompleteDirective,
-    DisableZipCodeDirective
+    DisableZipCodeDirective,
+    ProdResultsComponent
   ],
   imports: [
     BrowserModule,
