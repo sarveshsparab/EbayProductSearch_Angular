94d05c4fc1aa666b7cfde87f1cd5545633e46322
Sarvesh Parab <sparab@usc.edu>
2019-03-22 22:48:48 -0700

    vasic item details done 1. data received - just simple data 2. back button done

--- src/app/components/item-details/item-details.component.ts
+++ src/app/components/item-details/item-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {ItemDetailsService} from '../../services/item-details.service';
 
 
 @Component({
@@ -7,14 +8,38 @@ import { Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
   styleUrls: ['./item-details.component.css']
 })
 export class ItemDetailsComponent implements OnInit {
+  private itemDetailsTabs = [
+    { id: "productTab_itemDetails", title: "Product" },
+    { id: "photosTab_itemDetails", title: "Photos" },
+    { id: "shippingTab_itemDetails", title: "Shipping" },
+    { id: "sellerTab_itemDetails", title: "Seller" },
+    { id: "similarTab_itemDetails", title: "Similar Products" }
+  ];
+
   @Output() slide = new EventEmitter<string>();
 
-  constructor() {
+  private activeTab = "productTab_itemDetails";
+  itemDetailsData: any;
+  itemName: any;
 
+
+  constructor(private ids: ItemDetailsService) {
+    this.ids.itemDetailsDataOb.subscribe(data=> {
+      this.itemDetailsData = data;
+      this.itemName = data['title'][0];
+      console.log("888888888888888888888888888888888888888888888888");
+      console.log(data);
+    });
   }
 
   ngOnInit() {
-
   }
 
+  setActiveTab(id) {
+    this.activeTab = id;
+  }
+
+  backToListings() {
+    this.slide.emit("right");
+  }
 }
--- src/app/services/item-details.service.ts
+++ src/app/services/item-details.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {Subject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemDetailsService {
+  private itemDetailsDataSub = new Subject();
+  itemDetailsDataOb = this.itemDetailsDataSub.asObservable();
+
+  preFetchedItemDetailsData: any;
+
 
   constructor() { }
+
+  getAllItemDetails(){
+    this.itemDetailsDataSub.next(this.preFetchedItemDetailsData);
+  }
 }
