2a3ecaedbaaf270bd1918330743f74ef96e1a959
Sarvesh Parab <sparab@usc.edu>
2019-03-23 19:21:35 -0700

    leveraging the seller content class

--- src/app/components/item-details/seller-tab-details/seller-tab-details.component.ts
+++ src/app/components/item-details/seller-tab-details/seller-tab-details.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {SellerContent} from './SellerContent';
 
 @Component({
   selector: 'app-seller-tab-details',
@@ -6,7 +7,7 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./seller-tab-details.component.css']
 })
 export class SellerTabDetailsComponent implements OnInit {
-  @Input("sellerTab_content") content: any;
+  @Input("sellerTab_content") content: SellerContent;
 
   constructor() { }
 
