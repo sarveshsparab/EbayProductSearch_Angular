972ae534ffde743ef2d24ae2a58cf3e5e87ec206
Sarvesh Parab <sparab@usc.edu>
2019-03-24 00:21:37 -0700

    similiar items class made

---
+++ src/app/components/item-details/similar-items-tab-details/SimilarItemContent.ts
@@ -0,0 +1,9 @@
+export class SimilarItemContent {
+  Image_URL = '';
+  Product_Name = '';
+  Days_Left = '';
+  Price = '';
+  Shipping_Cost = '';
+  Response_Status = '';
+  Response_Message = '';
+}
