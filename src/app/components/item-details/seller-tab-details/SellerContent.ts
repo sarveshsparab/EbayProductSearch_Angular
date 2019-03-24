6ee680f6574c49694d414bd9d691771c5c9474e7
Sarvesh Parab <sparab@usc.edu>
2019-03-23 18:01:56 -0700

    seller content class made

---
+++ src/app/components/item-details/seller-tab-details/SellerContent.ts
@@ -0,0 +1,8 @@
+export class ShippingContent {
+  Feedback_Score: string;
+  Popularity: string;
+  Feedback_Rating_Star: string;
+  Top_Rated: string;
+  Store_Name: string;
+  Buy_Product_At: string;
+}
