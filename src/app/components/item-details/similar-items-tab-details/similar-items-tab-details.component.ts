5e4cdf4de7c439421e8b8bda5b9300b94660b637
Sarvesh Parab <sparab@usc.edu>
2019-03-24 12:02:20 -0700

    sorting done and basic look done for similar items tab

--- src/app/components/item-details/similar-items-tab-details/similar-items-tab-details.component.ts
+++ src/app/components/item-details/similar-items-tab-details/similar-items-tab-details.component.ts
@@ -1,17 +1,111 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit} from '@angular/core';
 import {SimilarItemContent} from './SimilarItemContent';
+import {min} from 'rxjs/operators';
 
 @Component({
   selector: 'app-similar-items-tab-details',
   templateUrl: './similar-items-tab-details.component.html',
   styleUrls: ['./similar-items-tab-details.component.css']
 })
-export class SimilarItemsTabDetailsComponent implements OnInit {
-  @Input("similarItemsTab_content") content: SimilarItemContent;
+export class SimilarItemsTabDetailsComponent implements OnInit, OnChanges {
+  @Input("similarItemsTab_content") content: SimilarItemContent[];
+
+  sortPref = [
+    "Default",
+    "Product Name",
+    "Days Left",
+    "Price",
+    "Shipping Cost"
+  ];
+  orderPref = [
+    "Ascending",
+    "Descending"
+  ];
+  simItemSortPref: any;
+  simItemOrderPref: any;
+  itemCount: number;
+  moreLessBtnLabel: any;
+  isMoreLessBtnNeeded: boolean;
+  orderSortDisabled: boolean;
+  orderedSimItems = new Array();
 
   constructor() { }
 
   ngOnInit() {
+    this.simItemSortPref = 0;
+    this.simItemOrderPref = 0;
+    this.itemCount = Math.min(5, this.content.length);
+    this.moreLessBtnLabel = 'Show More';
+    this.isMoreLessBtnNeeded = this.content.length >= 5;
+    this.orderSortDisabled = true;
   }
 
+  moreLessBtnClick() {
+    if(this.moreLessBtnLabel == 'Show More'){
+      this.moreLessBtnLabel = 'Show Less';
+      this.itemCount = this.content.length;
+    } else {
+      this.moreLessBtnLabel = 'Show More';
+      this.itemCount = Math.min(5, this.content.length);
+    }
+  }
+
+  sortItems() {
+    switch (this.simItemSortPref) {
+      case '0':
+        this.orderSortDisabled = true;
+        this.orderedSimItems = this.makeCopy(this.content);
+        break;
+      case '1':
+        this.orderedSimItems.sort(function (a, b) {
+          return a.Product_Name.localeCompare(b.Product_Name);
+        });
+        this.orderSortDisabled = false;
+        break;
+      case '2':
+        this.orderedSimItems.sort(function (a, b) {
+          return parseFloat(a.Days_Left)-parseFloat(b.Days_Left);
+        });
+        this.orderSortDisabled = false;
+        break;
+      case '3':
+        this.orderedSimItems.sort(function (a, b) {
+          return parseFloat(a.Price)-parseFloat(b.Price);
+        });
+        this.orderSortDisabled = false;
+        break;
+      case '4':
+        this.orderedSimItems.sort(function (a, b) {
+          return parseFloat(a.Shipping_Cost)-parseFloat(b.Shipping_Cost);
+        });
+        this.orderSortDisabled = false;
+        break;
+    }
+
+    if(!this.orderSortDisabled){
+      if(this.simItemOrderPref == '1')
+        this.orderedSimItems.reverse();
+    }
+  }
+
+  ngOnChanges(): void {
+    this.orderedSimItems = this.makeCopy(this.content);
+  }
+
+  makeCopy(original){
+    let newArray = [];
+    for (var i = 0; i < original.length; i++) {
+      let sic = new SimilarItemContent();
+      sic.Image_URL = original[i].Image_URL;
+      sic.Product_Name = original[i].Product_Name;
+      sic.Price = original[i].Price;
+      sic.Shipping_Cost = original[i].Shipping_Cost;
+      sic.Days_Left = original[i].Days_Left;
+      sic.Response_Status = original[i].Response_Status;
+      sic.Response_Message = original[i].Response_Message;
+
+      newArray.push(sic);
+    }
+    return newArray;
+  }
 }
--- src/app/services/similar-item-content.service.ts
+++ src/app/services/similar-item-content.service.ts
@@ -1,9 +1,119 @@
-import { Injectable } from '@angular/core';
+import {Injectable, EventEmitter, Output} from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {Subject, Observable} from 'rxjs';
+import {ProductContent} from '../components/item-details/product-tab-details/ProductContent';
+import {SimilarItemContent} from '../components/item-details/similar-items-tab-details/SimilarItemContent';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class SimilarItemContentService {
 
-  constructor() { }
+  private resultJsonSub = new Subject();
+  resultJsonOb = this.resultJsonSub.asObservable();
+  private jsonDataFetched: any;
+
+  constructor(private http: HttpClient) {
+  }
+
+  fetchSimilarItems(itemId) {
+    let params = new HttpParams()
+      .set('itemid', itemId);
+
+    let response = this.http.get(this.buildEbayUrl(itemId));
+
+    response.subscribe(
+      data => {
+
+        console.log(data);
+
+        this.jsonDataFetched = data;
+        let responseValidityCheck = this.isFetchedResponseValid(this.jsonDataFetched);
+        if (!responseValidityCheck[0]) {
+          let simCont = new SimilarItemContent();
+          simCont.Response_Status = 'Error';
+          simCont.Response_Message = responseValidityCheck[1] + '';
+          this.resultJsonSub.next(simCont);
+        } else {
+
+          let simItemObjsArray = new Array();
+
+          let similarItems = this.jsonDataFetched.getSimilarItemsResponse.itemRecommendations.item;
+          for (let s = 0; s < similarItems.length; s++) {
+            let simCont = new SimilarItemContent();
+            simCont.Response_Status = 'Success';
+
+            if (similarItems[s].imageURL != null) {
+              simCont.Image_URL = similarItems[s].imageURL;
+            }
+
+            if (similarItems[s].title != null) {
+              simCont.Product_Name = similarItems[s].title;
+            }
+
+            if (similarItems[s].buyItNowPrice != null) {
+              simCont.Price = similarItems[s].buyItNowPrice.__value__;
+            }
+
+            if (similarItems[s].shippingCost != null) {
+              simCont.Shipping_Cost = similarItems[s].shippingCost.__value__;
+            }
+
+            if (similarItems[s].timeLeft != null) {
+              simCont.Days_Left = similarItems[s].timeLeft.substring(1,similarItems[s].timeLeft.indexOf("D"));
+            }
+
+            simItemObjsArray.push(simCont);
+          }
+
+          console.log(simItemObjsArray);
+
+          this.resultJsonSub.next(simItemObjsArray);
+
+        }
+      });
+  }
+
+  buildEbayUrl(itemId) {
+
+    let tempEbayUrl = 'http://svcs.ebay.com/MerchandisingService?';
+    tempEbayUrl += 'OPERATION-NAME=getSimilarItems';
+    tempEbayUrl += '&SERVICE-NAME=MerchandisingService';
+    tempEbayUrl += '&SERVICE-VERSION=1.1.0';
+    tempEbayUrl += '&CONSUMER-ID=' + 'SarveshP-sarveshp-PRD-4a6d4ee64-9b547e7c';
+    tempEbayUrl += '&RESPONSE-DATA-FORMAT=JSON';
+    tempEbayUrl += '&REST-PAYLOAD';
+    tempEbayUrl += '&itemId=' + itemId;
+    tempEbayUrl += '&maxResults=20';
+
+    console.log(tempEbayUrl);
+
+    return tempEbayUrl;
+  }
+
+  private isFetchedResponseValid(jsonObj) {
+    let errMsg = '';
+    let retrievedValidSimilarItems = true;
+    if (jsonObj == null || jsonObj.length == 0) {
+      retrievedValidSimilarItems = false;
+      errMsg = 'No Records';
+    } else if (jsonObj.getSimilarItemsResponse == null) {
+      retrievedValidSimilarItems = false;
+      errMsg = 'No Records';
+    } else if (jsonObj.getSimilarItemsResponse.itemRecommendations == null) {
+      retrievedValidSimilarItems = false;
+      errMsg = 'No Records';
+    } else if (jsonObj.getSimilarItemsResponse.itemRecommendations.item == null
+      || jsonObj.getSimilarItemsResponse.itemRecommendations.item.length == 0) {
+      retrievedValidSimilarItems = false;
+      errMsg = 'No Records';
+    }
+    return [retrievedValidSimilarItems, errMsg];
+
+
+  }
 }
