ab83022d3dbfc26a66418e4681ec9eabec18a477
Sarvesh Parab <sparab@usc.edu>
2019-03-23 17:55:17 -0700

    shipping content class made

---
+++ src/app/components/item-details/shipping-tab-details/ShippingContent.ts
@@ -0,0 +1,8 @@
+export class ShippingContent {
+  Shipping_Cost: string;
+  Shipping_Locations: string;
+  Handling_Time: string;
+  Expedited_Shipping: boolean;
+  One_Day_Shipping: boolean;
+  Return_Accepted: boolean;
+}
