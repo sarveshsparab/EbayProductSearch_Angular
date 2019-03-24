995480b2d9b463060809c9f61d810296bc9abb83
Sarvesh Parab <sparab@usc.edu>
2019-03-23 21:48:32 -0700

    product content class fixed

--- src/app/components/item-details/product-tab-details/ProductContent.ts
+++ src/app/components/item-details/product-tab-details/ProductContent.ts
@@ -1,10 +1,10 @@
 export class ProductContent {
-  Product_Images: [];
-  Subtitle: string;
-  Price: string;
-  Location: string;
-  Return_Policy: string;
-  Item_Specifics: [];
-  Response_Status: string;
-  Response_Message: string;
+  Product_Images = new Array();
+  Subtitle = '';
+  Price = '';
+  Location = '';
+  Return_Policy = '';
+  Item_Specifics = new Array();
+  Response_Status = '';
+  Response_Message = '';
 }
