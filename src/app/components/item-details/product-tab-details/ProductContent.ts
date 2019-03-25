d04beaf871d89d61ac08dc8dd48159b87c9432ba
Sarvesh Parab <sparab@usc.edu>
2019-03-24 17:47:46 -0700

    added title and link to productContent class

--- src/app/components/item-details/product-tab-details/ProductContent.ts
+++ src/app/components/item-details/product-tab-details/ProductContent.ts
@@ -1,10 +1,12 @@
 export class ProductContent {
   Product_Images = new Array();
+  Title = '';
   Subtitle = '';
   Price = '';
   Location = '';
   Return_Policy = '';
   Item_Specifics = new Array();
+  Link = '';
   Response_Status = '';
   Response_Message = '';
 }
