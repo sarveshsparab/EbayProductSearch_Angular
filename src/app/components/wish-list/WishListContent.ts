ee6139cd0adf60dc69e8b85378f5f1cfb3298ffb
Sarvesh Parab <sparab@usc.edu>
2019-03-25 12:00:27 -0700

    wishlist content class made

---
+++ src/app/components/wish-list/WishListContent.ts
@@ -0,0 +1,15 @@
+import {SellerContent} from '../item-details/seller-tab-details/SellerContent';
+import {ShippingContent} from '../item-details/shipping-tab-details/ShippingContent';
+
+export class WishListContent {
+  ItemID = '';
+  Image_URL = '';
+  Title = '';
+  Price = '';
+  Shipping_Option = '';
+  Seller_Name = '';
+  Seller_Content_Obj: SellerContent;
+  Shipping_Content_Obj: ShippingContent;
+  Response_Status = '';
+  Response_Message = '';
+}
