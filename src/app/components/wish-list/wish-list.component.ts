3543e33c8bb5633290a1dffd83eb939ac773ab1b
Sarvesh Parab <sparab@usc.edu>
2019-03-26 01:03:09 -0700

    wiwishList comp. ts done
    Pending - show already slected row!

--- src/app/components/wish-list/wish-list.component.ts
+++ src/app/components/wish-list/wish-list.component.ts
@@ -1,4 +1,6 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {WishListService} from '../../services/wish-list.service';
+import {ItemDetailsService} from '../../services/item-details.service';
 
 @Component({
   selector: 'app-wish-list',
@@ -6,11 +8,80 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
   styleUrls: ['./wish-list.component.css']
 })
 export class WishListComponent implements OnInit {
-  @Output() slide = new EventEmitter<string>();
+  @Output() slide = new EventEmitter<any>();
+  @Input("itemInfo") selectedItemInfo: any;
+  @Input("itemId") selectedItemId: any;
 
-  constructor() { }
+  wishListArray: any;
+  isAnyWishListItem: boolean;
+  error_msg: any;
 
-  ngOnInit() {
+  constructor(private wls: WishListService, private ids: ItemDetailsService) {
+    this.wls.wishListOb.subscribe(data => {
+      this.wishListArray = data;
+      console.log(data);
+      if(this.wishListArray.length <= 1){
+        if(this.wishListArray[0].Response_Status == 'Error'){
+          this.isAnyWishListItem = false;
+          this.error_msg = this.wishListArray[0].Response_Message;
+        } else {
+          this.isAnyWishListItem = true;
+          this.error_msg = 'Hi2';
+        }
+      } else {
+        this.isAnyWishListItem = true;
+        this.error_msg = 'Hi';
+      }
+    });
   }
 
+  removeFromWishList(wlObj){
+    this.wls.removeFromWishList(wlObj);
+  }
+
+  ngOnInit() {
+    this.loadEntireWishList();
+  }
+
+  private loadEntireWishList() {
+    this.wls.fetchEntireWishList();
+  }
+
+  getItemDetails() {
+    this.ids.getAllItemDetailsForWishList(this.selectedItemInfo);
+    this.slide.emit({slide: 'left', itemId: this.selectedItemInfo.ItemId, itemInfo: this.selectedItemInfo});
+  }
+
+  fetchItemDetails(rowdata) {
+    this.selectedItemInfo = rowdata;
+    this.getItemDetails();
+  }
+
+  processTitle(title) {
+    if (title == null || title.length == 0 || title=='N/A') {
+      return 'N/A';
+    } else {
+      let titleLen = title.length;
+      if (titleLen < 35) {
+        return title;
+      } else {
+        if (title.substring(0, 35).slice(-1) == ' ') {
+          return title.substring(0, 35) + '...';
+        } else {
+          return title.substring(0, title.substring(0, 35).lastIndexOf(' ')) + '...';
+        }
+      }
+    }
+  }
+
+  processImage(Image_URL) {
+    if (Image_URL != "N/A") {
+      let imageElem = document.createElement('img');
+      imageElem.setAttribute('src', Image_URL);
+      imageElem.setAttribute('style', 'height: 135px; width: 135px;');
+      return imageElem.outerHTML;
+    } else {
+      return 'N/A';
+    }
+  }
 }
